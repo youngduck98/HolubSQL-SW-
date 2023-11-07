71ae7c3f5c67f8f945bb735f31f1caad0710b324
Broco98 <qkrgydus00@naver.com>
2023-11-07 17:50:55 +0900

    update/UnUpdatable 생성자 추가

--- src/com/holub/database/UnUpdatable.java
+++ src/com/holub/database/UnUpdatable.java
@@ -2,6 +2,10 @@ package com.holub.database;
 
 public class UnUpdatable extends GrantDecorator{
 
+    public UnUpdatable(Table wrapped){
+        this.wrapped = wrapped;
+    }
+
     private final void illegal()    {
         throw new UnsupportedOperationException();
     }
