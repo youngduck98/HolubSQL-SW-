2b8c85cb837eece061305df7fbf35804645ecbf4
youngduck98 <tommykimyd98@gmail.com>
2023-11-27 22:47:39 +0900

    add load/save table in dao

--- src/com/holub/application/dao/Dao.java
+++ src/com/holub/application/dao/Dao.java
@@ -1,21 +1,31 @@
 package com.holub.application.dao;
 
+import java.io.*;
+import java.util.Iterator;
 import java.util.List;
-import com.holub.database.Table;
+
+import com.holub.database.*;
+import com.holub.tools.ArrayIterator;
 
 public abstract class Dao {
 
     Table table;
     String baseUrl;
 
-    public Dao(String url){
+    public Dao(String url, String extension){
         baseUrl = url;
     }
 
-    public void saveTable() { // expoter 사용해서 파일로 저장
+    public void saveTable() throws IOException { // expoter 사용해서 파일로 저장
+        Writer out = new FileWriter(table.name() + "." + "csv");
+        Table.Exporter exporter = new CSVExporter(out);
+        table.export(exporter);
         // TODO
     }
-    public void loadTable(String tableName) { // impoter 사용해서 파일 로드 -> table 생성
+    public void loadTable(String tableName) throws IOException { // impoter 사용해서 파일 로드 -> table 생성
+        Reader in = new FileReader(tableName + ".csv");
+        table = TableFactory.create(new CSVImporter(in));
+        in.close();
         // TODO
     }
 
