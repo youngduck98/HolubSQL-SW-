b269369fafd2dbac15357864be72e435d93f4080
Broco98 <qkrgydus00@naver.com>
2023-11-28 05:18:03 +0900

    update) BookServiceImpl model을 사용하도록 구조 변환

--- src/com/holub/application/service/book/BookServiceImpl.java
+++ src/com/holub/application/service/book/BookServiceImpl.java
@@ -4,6 +4,7 @@ import com.holub.application.dao.Dao;
 import com.holub.application.domain.book.Book;
 import com.holub.application.domain.book.CheckOutState;
 import com.holub.application.domain.member.Grant;
+import com.holub.application.domain.member.Member;
 import com.holub.application.model.Model;
 
 import java.io.IOException;
@@ -14,15 +15,17 @@ import java.util.List;
 public class BookServiceImpl implements BookService {
 
     private static BookService instance;
-    private final Dao dao;
+    private final Dao bookDao;
+    private final Dao memberDao;
 
-    private BookServiceImpl(Dao dao){
-        this.dao = dao;
+    private BookServiceImpl(Dao bookDao, Dao memberDao){
+        this.bookDao = bookDao;
+        this.memberDao = memberDao;
     }
 
-    public static BookService getInstance(Dao dao){
+    public static BookService getInstance(Dao bookDao, Dao memberDao){
         if (instance == null) {
-            instance = new BookServiceImpl(dao);
+            instance = new BookServiceImpl(bookDao, memberDao);
         }
         return instance;
     }
@@ -30,34 +33,54 @@ public class BookServiceImpl implements BookService {
     @Override
     public List<Book> getBookList(Model model) {
         List<Book> list = new ArrayList<>();
-        
-        for (Object o : dao.selectTable(uuids, callName, asc)) {
+
+        List<Integer> uuidList = (List<Integer>) model.getAttribute("uuidList");
+        String[] callName = (String[]) model.getAttribute("callName");
+        int[] asc = (int[]) model.getAttribute("asc");
+
+        for (Object o : bookDao.selectTable(uuidList, callName, asc)) {
             list.add((Book) o);
         }
+
+        model.clearAttribute();
         return list;
     }
 
     @Override
     public void addBook(Model model) {
-        if (grant == Grant.Manager) {
-            dao.insertTable(Arrays.asList(new Book[] {bookInfo}));
+        Grant grant = getMyGrant(model);
+        Book bookInfo = (Book) model.getAttribute("addBookInfo");
+
+        if (bookInfo.getUuid() == -1 && grant == Grant.Manager){
+            bookDao.insertTable(Arrays.asList(new Book[] {bookInfo}));
         }
+
+        model.clearAttribute();
     }
 
     @Override
     public void deleteBook(Model model) throws IOException {
+        Grant grant = getMyGrant(model);
+
         if (grant == Grant.Manager) {
-            Book book = getBookList(Arrays.asList(new Integer[] {uuid}), null, null).get(0);
+            Book book = getBookList(model).get(0);
             book.setCheckOutState(CheckOutState.Deleted);
-            dao.updateTable(book);
+            bookDao.updateTable(book);
         }
+
+        model.clearAttribute();
     }
 
     @Override
     public void modifyBookInfo(Model model) throws IOException{
+        Grant grant = getMyGrant(model);
+        Book bookInfo = (Book) model.getAttribute("modifyBookInfo");
+
         if (grant == Grant.Manager) {
-            dao.updateTable(bookInfo);
+            bookDao.updateTable(bookInfo);
         }
+
+        model.clearAttribute();
     }
 
     @Override
@@ -70,5 +93,12 @@ public class BookServiceImpl implements BookService {
     public List<Book> getOnlySpecialGenre(Model model) {
         // TODO
         return null;
+    }
+
+    private Grant getMyGrant(Model model) {
+        Integer myUuid = (Integer) model.getAttribute("myInfo");
+        Member myMember = (Member) memberDao.selectTable(
+                Arrays.asList(new Integer[]{myUuid}), null, null).get(0);
+        return myMember.getGrant();
     }
 }
