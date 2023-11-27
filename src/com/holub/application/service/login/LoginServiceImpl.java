742791ec53b6a774c381feb3efbfddf7cd31ba06
Broco98 <qkrgydus00@naver.com>
2023-11-28 01:52:13 +0900

    feat) service 구조 셋팅

--- src/com/holub/application/service/login/LoginServiceImpl.java
+++ src/com/holub/application/service/login/LoginServiceImpl.java
@@ -1,4 +1,37 @@
 package com.holub.application.service.login;
 
+import com.holub.application.dao.Dao;
+import com.holub.application.model.Model;
+
 public class LoginServiceImpl implements LoginService{
+
+    private static LoginService instance;
+    private final Dao dao;
+
+    private LoginServiceImpl(Dao dao){
+        this.dao = dao;
+    }
+
+    public static LoginService getInstance(Dao dao){
+        if (instance == null) {
+            instance = new LoginServiceImpl(dao);
+        }
+        return instance;
+    }
+
+    @Override
+    public void login(Model model, String id, String password) {
+        // TODO
+        // 1. dao에서 id, password 탐색
+        // 2. Member 변수 생성
+        // 3. Model에 저장
+        // model.addAttribute("MyInfo", member);
+
+    }
+
+    @Override
+    public void logout(Model model) {
+        // TODO
+        model.removeAttribute("MyInfo");
+    }
 }
