cd1feb74847cddb727995316f74c8cfc2705ad7a
Broco98 <qkrgydus00@naver.com>
2023-11-28 02:31:20 +0900

    feat) Config 구조 구현

--- src/com/holub/application/Config.java
+++ src/com/holub/application/Config.java
@@ -1,4 +1,48 @@
 package com.holub.application;
 
+import com.holub.application.dao.BookDao;
+import com.holub.application.dao.CheckOutDao;
+import com.holub.application.dao.MemberDao;
+import com.holub.application.domain.member.Member;
+import com.holub.application.service.book.BookService;
+import com.holub.application.service.book.BookServiceImpl;
+import com.holub.application.service.checkout.CheckOutService;
+import com.holub.application.service.checkout.CheckOutServiceImpl;
+import com.holub.application.service.login.LoginService;
+import com.holub.application.service.login.LoginServiceImpl;
+import com.holub.application.service.member.MemberService;
+import com.holub.application.service.member.MemberServiceImpl;
+
+import java.io.IOException;
+
 public class Config {
+
+    public BookService getBookService() throws IOException {
+        return BookServiceImpl.getInstance(BookDao.getInstance(
+                "",
+                ""
+        ));
+    }
+
+    public CheckOutService getCheckOutService() throws IOException {
+        return CheckOutServiceImpl.getInstance(CheckOutDao.getInstance(
+                "",
+                ""
+        ));
+    }
+
+    public LoginService getLoginService() throws IOException {
+        return LoginServiceImpl.getInstance(MemberDao.getInstance(
+                "",
+                ""
+        ));
+    }
+
+    public MemberService getMemberService() throws IOException {
+        return MemberServiceImpl.getInstance(MemberDao.getInstance(
+                "",
+                ""
+        ));
+    }
+
 }
