b79b6804b68eebeedd741e0a25334ed3bf9d4c46
김효연_T5072 <39620398+Broco98@users.noreply.github.com>
2023-11-29 03:08:15 +0900

    feat) MainNoneView 구현 (임시)

--- src/com/holub/application/view/MainNoneView.java
+++ src/com/holub/application/view/MainNoneView.java
@@ -1,7 +1,32 @@
 package com.holub.application.view;
 
-public class MainNoneView {
+import com.holub.application.model.Model;
+import com.holub.application.util.InputStorage;
 
-   //TODO
+public class MainNoneView implements View{
+
+    // 메뉴 출력
+    public void showMenu() {
+        System.out.println("(1)회원가입 (2)로그인 (3)책 검색");
+        System.out.println("([)이전 페이지  (])다음 페이지");
+    }
+
+    // 책 리스트 출력
+    public void showBookList(Model model) {
+        // TODO
+        // model에서 Book 정보 찾아서 출력, 단 View는 Book을 몰라야함 -> 즉, Model에 넣을 때 Book객체가 아니라 List<String>등으로 바꿔서 넣을것
+    }
+    
+    // 입력 받기
+    public void getMenu() {
+        storage.getInput();
+    }
+
+    @Override
+    public void execute(Model model) {
+        showMenu();
+        showBookList(model);
+        getMenu();
+    }
 
 }
